7ff7515980ca74bcd647dd60c92731c60fac16b8
Pelayo Parejo Pagador <pelayo.parejopagador@epitech.eu>
2014-02-02 13:45:45 +0000

    Profile beta 1

--- ProjectWeek/src/main/java/com/example/projectweek/FragmentAccount.java
+++ ProjectWeek/src/main/java/com/example/projectweek/FragmentAccount.java
@@ -1,10 +1,12 @@
 package com.example.projectweek;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by svirch_n on 01/02/14.
@@ -12,8 +14,13 @@ import android.view.ViewGroup;
  */
 public class FragmentAccount extends Fragment {
 
+    Typeface tp = null;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        View view = inflater.inflate(R.layout.fragment_account, container, false);
+        tp = Typeface.createFromAsset(getActivity().getAssets(), "fonts/Roboto-Bold.ttf");
+        ((TextView) view.findViewById(R.id.profileEditTxt)).setTypeface(tp);
+        return view;
     }
 }
